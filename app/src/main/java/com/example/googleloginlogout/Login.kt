042f322a6ba7c00799fa7755a6f073b906104609
Sibrina <17603312@vegaconnect.co.za>
2021-05-12 10:28:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/googleloginlogout/Login.kt
@@ -0,0 +1,100 @@
+package com.example.googleloginlogout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+const val DEFVALS = "com.example.googleloginlogout.MESSAGE";
+
+class Login : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance();
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        if(currentUser != null){
+            Log.d(TAG, currentUser.displayName.toString())
+        }
+        val signUp = findViewById<Button>(R.id.button)
+        val username = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val signIn = findViewById<Button>(R.id.button2)
+
+        signUp.setOnClickListener {
+            createAccount(username.text.toString(), password.text.toString())
+        }
+
+        signIn.setOnClickListener {
+           login(username.text.toString(), password.text.toString())
+        }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) { val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(DEFVALS, currentUser)
+        }
+            startActivity(intent)
+            Log.d(TAG, currentUser.displayName.toString())
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = mAuth!!.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun login(email: String, password: String)
+    {
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = mAuth!!.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+
+
+}
